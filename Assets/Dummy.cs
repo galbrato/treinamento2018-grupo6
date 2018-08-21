2bc30edc8f84062fd06e6c288d378bea20088a44
Chieppe <chieppe2142@gmail.com>
2018-08-21 09:09:51 -0300

    Controle personagem
    Atualizacao do script de controle para se comportar corretamente quando o jogo pausar nos interativeis

---
+++ Assets/Dummy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1")){
+            Time.timeScale=0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if(Input.GetButton("Fire2")){
+            Time.timeScale=1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+	}
+}
