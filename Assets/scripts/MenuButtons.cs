d846b9672e473d3bd2211a9ad36c5ffe63195a02
vini <vini_carvalho99@hotmail.com>
2018-08-25 17:20:50 -0300

    adicionei mais botoes ao menu e terminei de configuras as funcoes do menu

---
+++ Assets/scripts/MenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
