c3e85bd593971f808366272ef8d4214d87838a79
vini <vini_carvalho99@hotmail.com>
2018-08-27 15:54:31 -0300

    scripts e audios que nao tinha enviado

---
+++ Assets/scripts/telefone.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class telefone : interagivel {
+    public bool toca;
+    public AudioSource som;
+    public bool play;
+
+
+    void Start()
+    {
+        som = gameObject.GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+
+        if (utiliz)
+        {
+            fazer();
+        }
+    }
+
+    public override void fazer()
+    {
+        if (toca)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                toca = false;
+                som.Pause();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                toca = true;
+                if (play)
+                {
+                    som.UnPause();
+                }
+                else
+                {
+                    som.Play();
+                }
+            }
+        }
+
+    }
+
+    public void OnGUI()
+    {
+        if (utiliz)
+        {
+            if (toca)
+            {
+                GUI.Box(new Rect(0, 300, 600, 25), "Não atendo mais isso; Eles só estão esperando eu desistir e falar tudo que eu sei...");
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to turn off");
+            }
+            else
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to interact");
+            }
+        }
+    }
+
+}
